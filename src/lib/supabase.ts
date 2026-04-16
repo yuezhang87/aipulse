f340122b2189957180962fef7a47af7b1ada6439
yuezhang87 <43572171+yuezhang87@users.noreply.github.com>
2026-04-16 19:09:36 +0000

    Fix Supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://zlnmosacddraebxsofel.supabase.co',
-  'sb_publishable_YucSv5gQyL16jVlX67Ws3g_rkqQzvy3'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpsbm1vc2FjZGRyYWVieHNvZmVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyOTk0NzQsImV4cCI6MjA5MTg3NTQ3NH0.SoArHOW1SqWbJy780wm3x-R_qSHjxupDXE_yWSu6Tvk'
 )
